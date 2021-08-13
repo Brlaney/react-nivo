9fe61f773a60d755434688975eac379fd927a3ec
brlaney <brlaney@outlook.com>
2021-08-12 21:16:15 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ChordGraph from './components/ChordGraph'
+// import Carousel from './components/Carousel';
+import './scss/App.scss';
+import { data } from './lib/data/chord'
+
+
+
+function App() {
+  return (
+    <div className='App'>
+      <div
+        id='container'
+        className='uk-flex uk-flex-middle uk-flex-column uk-text-center'
+      >
+        <div id='chart-container' className='uk-card uk-card-default uk-card-body'>
+          <div id='grid' className='uk-grid-small uk-child-width-expand@s uk-text-center' uk-grid>
+
+            {/* Grid card 1 */}
+            <div id='parent'>
+              <div id='graph-container' className='uk-card uk-card-default uk-card-body'>
+                <ChordGraph data={data} />
+              </div>
+            </div>
+
+            {/* Grid card 2 */}
+            <div id='parent'>
+              <div id='graph-container' className='uk-card uk-card-default uk-card-body'>
+                More charts
+              </div>
+            </div>
+
+            {/* Grid card 3 */}
+            <div id='parent'>
+              <div id='graph-container' className='uk-card uk-card-default uk-card-body'>
+                More charts
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/data/bar/index.ts
@@ -0,0 +1,9 @@
+export const data = [
+  { state: 'TN', deaths: 119, deathsColor: 'hsl(266, 70%, 50%)' },
+  { state: 'TX', deaths: 39, deathsColor: 'hsl(355, 70%, 50%)' },
+  { state: 'FL', deaths: 196, deathsColor: 'hsl(59, 70%, 50%)' },
+  { state: 'NY', deaths: 167, deathsColor: 'hsl(92, 70%, 50%)' },
+  { state: 'OH', deaths: 168, deathsColor: 'hsl(180, 70%, 50%)' },
+  { state: 'GA', deaths: 84, deathsColor: 'hsl(344, 70%, 50%)' },
+  { state: 'CA', deaths: 48, deathsColor: 'hsl(299, 70%, 50%)' }
+];
